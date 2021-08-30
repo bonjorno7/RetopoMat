1a08e194f91ab2caee27e6dac617b5650c6d3b6a
bonjorno7 <jorijndegraaf@gmail.com>
2021-08-30 17:45:29 +0200

    Add material ops
    I was going to have a single class for adding materials, but it turned out a bit messy, so I'm just doing two operators now.
    The advantage is that you can have a separate label and description for each, which simplifies the UI code.

---
+++ ops.py
@@ -0,0 +1,75 @@
+from bpy.types import Context, Operator
+from bpy.utils import register_class, unregister_class
+
+from .utils import assign_material, get_reference_material, get_retopo_material
+
+
+class AddReferenceMaterialOperator(Operator):
+    bl_idname = 'retopomat.add_reference_material'
+    bl_label = 'Add Reference Material'
+    bl_description = 'Add a reference material to the selected objects'
+    bl_options = {'REGISTER', 'INTERNAL', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context: Context) -> bool:
+        return any(obj.type == 'MESH' for obj in context.selected_objects)
+
+    def execute(self, context: Context) -> set:
+        material = get_reference_material()
+        assign_material(context.selected_objects, material)
+
+        self.report({'INFO'}, 'Added reference material')
+        return {'FINISHED'}
+
+
+class AddRetopoMaterialOperator(Operator):
+    bl_idname = 'retopomat.add_retopo_material'
+    bl_label = 'Add Retopo Material'
+    bl_description = 'Add a retopo material to the selected objects'
+    bl_options = {'REGISTER', 'INTERNAL', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context: Context) -> bool:
+        return any(obj.type == 'MESH' for obj in context.selected_objects)
+
+    def execute(self, context: Context) -> set:
+        material = get_retopo_material()
+        assign_material(context.selected_objects, material)
+
+        self.report({'INFO'}, 'Added retopo material')
+        return {'FINISHED'}
+
+
+class RemoveMaterialsOperator(Operator):
+    bl_idname = 'retopomat.remove_materials'
+    bl_label = 'Remove Materials'
+    bl_description = 'Remove all materials from the selected objects'
+    bl_options = {'REGISTER', 'INTERNAL', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context: Context) -> bool:
+        return any(obj.type == 'MESH' for obj in context.selected_objects)
+
+    def execute(self, context: Context) -> set:
+        material = None  # Only remove materials.
+        assign_material(context.selected_objects, material)
+
+        self.report({'INFO'}, 'Cleared materials')
+        return {'FINISHED'}
+
+
+classes = (
+    AddReferenceMaterialOperator,
+    AddRetopoMaterialOperator,
+    RemoveMaterialsOperator,
+)
+
+
+def register():
+    for cls in classes:
+        register_class(cls)
+
+
+def unregister():
+    for cls in reversed(classes):
+        unregister_class(cls)
